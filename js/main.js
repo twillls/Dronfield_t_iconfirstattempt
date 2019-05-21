3a77621e5019d7c5840e9767894478b251b8ccf4
tdronfield <t_dronfield@fanshaweonline.ca>
2019-05-21 15:59:33 -0400

    adding more js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@
   //setting up a variable using the let keyword,
   //and using a CSS selector to find the element we want to use
 
-  //let stores the referene to element aws a variable (in memory)
-  let svgGraphic = document.querySelector("#badgeSVG");
+  //"let" stores the referene to element as a variable (in memory) we can call to it
+  let svgGraphic = document.querySelector("#badgeSVG"),
+      mainHeadline = document.querySelector(".main-headline"),
+      svgHeadline = document.querySelector(".svgHeading");
 
   //functions are reusable pieces of code that you can run any time
   function logMyId() {
@@ -17,4 +19,7 @@
 
   svgGraphic.addEventListener("click", logMyId);
 
+  mainHeadline.textContent = "Now you're something else!";
+
+  svgHeadline.textContent = "We're switching it Up!";
 })();
